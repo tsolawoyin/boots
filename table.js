99670f5877b96ddc57973244bcb8426785d966a1
tsolawoyin <t.olawoyin@outlook.com>
2024-01-16 23:52:43 +0100

    started project
    a mini website to display result of students. Just to make everyone accountable
    having results displayed to everyone will make everyone on their toes, especially Seun

---
+++ table.js
@@ -0,0 +1,14 @@
+let tableContents = [
+    {
+        date: "20-01-2024",
+        subject: "chm",
+        topic: "atomic structure",
+        "highest performer": null
+    },
+    {
+        date: "20-01-2024",
+        subject: "eng",
+        topic: "synonym I",
+        "highest performer": null
+    },
+]
