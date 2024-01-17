12e6527e5d55449c7adbfb5fbf335c40e1753e2f
tsolawoyin <t.olawoyin@outlook.com>
2024-01-17 01:13:27 +0100

    modified

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 // declarations
 const tableContentsEl = document.querySelector("table-of-contents");
-const tbodyEl = document.querySelector("tbody")
+const tbodyEl = document.querySelector("#tbody")
 
 
 
 // simple function that will help to append stuff
 // structure of content is 
 
-let i = [
-    {
-        date: "20-01-2024",
-        subject: "chm",
-        topic: "atomic structure",
-        "highest performer": null
-    },
-    {
-        date: "20-01-2024",
-        subject: "eng",
-        topic: "synonyms I",
-        "highest performer": null
-    },
-]
+makeTable(tableContents, tbodyEl);
+
+// let i = [
+//     {
+//         date: "20-01-2024",
+//         subject: "chm",
+//         topic: "atomic structure",
+//         "highest performer": null
+//     },
+//     {
+//         date: "20-01-2024",
+//         subject: "eng",
+//         topic: "synonyms I",
+//         "highest performer": null
+//     },
+// ]
 // el is tbody element
 function makeTable(contents, el) {
     for (let c of contents) {
         el.innerHTML += 
         `<tr>
-            <td>${c.date}</td>
+            <th scope="row">${c.date}</th>
             <td>${c.subject}</td>
             <td>${c.topic}</td>
             <td>${c["highest performer"] ? c["highest performer"] : "pending"}</td>
+            <td>${c.score ? c.score : ""}</td>
         </tr>`
     }
 
     return "done"
 }
-
-makeTable(i, tbodyEl)
+// makeTable(i, tbodyEl)
 
