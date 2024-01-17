9d89a8e51c7f2961af03f74868eef3bc1ddf4f41
tsolawoyin <t.olawoyin@outlook.com>
2024-01-17 07:13:51 +0100

    made some changes'

--- table.js
+++ table.js
@@ -1,5 +1,12 @@
 let tableContents = [
     {
+        date: "13-01-2024",
+        subject: "chm",
+        topic: "nitrogen",
+        "highest performer": "Seun",
+        score: "90%"
+    },
+    {
         date: "20-01-2024",
         subject: "chm",
         topic: "atomic structure",
@@ -11,4 +18,65 @@ let tableContents = [
         topic: "synonym I",
         "highest performer": null
     },
-]
+    {
+        date: "26-01-2024",
+        subject: "chm",
+        topic: "chemical bonding",
+        "highest performer": null
+    },
+    {
+        date: "01-02-2024",
+        subject: "chm",
+        topic: "carbon",
+        "highest performer": null
+    },
+    {
+        date: "08-02-2024",
+        subject: "chm",
+        topic: "air and air pollution",
+        "highest performer": null
+    },
+    {
+        date: "16-02-2024",
+        subject: "chm",
+        topic: "sulfur",
+        "highest performer": null
+    },
+    {
+        date: "16-02-2024",
+        subject: "chm",
+        topic: "oxygen",
+        "highest performer": null
+    },
+    {
+        date: "23-02-2024",
+        subject: "chm",
+        topic: "hydrogen",
+        "highest performer": null
+    },
+    {
+        date: "06-03-2024",
+        subject: "chm",
+        topic: "water and water pollution",
+        "highest performer": null
+    },
+    {
+        date: "16-03-2024",
+        subject: "chm",
+        topic: "halogens",
+        "highest performer": null
+    },
+    {
+        date: "03-04-2024",
+        subject: "chm",
+        topic: "metals",
+        "highest performer": null
+    },
+    {
+        date: "15-04-2024",
+        subject: "chm",
+        topic: "transition metal",
+        "highest performer": null
+    },
+
+]   
