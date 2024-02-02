4969b73e19d8a6a665933fc19d405c77d778fbe8
tsolawoyin <t.olawoyin@outlook.com>
2024-02-02 18:50:38 +0100

    update leaderboard

--- exam/js/validateUser.js
+++ exam/js/validateUser.js
@@ -1,6 +1,6 @@
 // this code will help validate
 
-let validUsers = ["adekizzy", "danny", "sen", "victoria", "temmy"]
+let validUsers = ["adekizzy", "danny", "sen", "victoria", "temmy", 'olamide']
 
 // return true if user is present
 function valid(user) {
--- leaderboard.js
+++ leaderboard.js
@@ -44,9 +44,9 @@ const leaderBoard= [
     {
         posn: 2,
         name: "Seun",
-        chm: [50,25,32.5,17.5,57.5,47.5,92.5], // an object will help me track their progress in each topic
+        chm: [67.5,50,25,32.5,17.5,57.5,47.5,92.5], // an object will help me track their progress in each topic
         bio: [0],
-        eng: [30,95,72.5,-12.5,20], 
+        eng: [100,30,95,72.5,-12.5,20], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
@@ -54,9 +54,9 @@ const leaderBoard= [
     {
         posn: 3,
         name: "Ayodeji",
-        chm: [27.5,22.5,70,22.5,72.5,90,62.5],
+        chm: [62,5,27.5,22.5,70,22.5,72.5,90,62.5],
         bio: [0],
-        eng: [30,0,52.5,5,10], 
+        eng: [40,30,0,52.5,5,10], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
@@ -74,9 +74,9 @@ const leaderBoard= [
     {
         posn: 1,
         name: "Tosin",
-        chm: [55,45,50,17.5,90,40,67.5],
+        chm: [90,55,45,50,17.5,90,40,67.5],
         bio: [0],
-        eng: [95,62.5,72.5,55,32.5], 
+        eng: [100,95,62.5,72.5,55,32.5], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
