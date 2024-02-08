0a17ebe28f9e3bacbb08294b7ad3f040fce2f47e
tsolawoyin <t.olawoyin@outlook.com>
2024-02-08 19:55:47 +0100

    removed render

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -3,9 +3,9 @@ const leaderBoard= [
     {
         posn: 2,
         name: "Seun",
-        chm: [23,32.5,67.5,50,25,32.5,17.5,57.5,47.5,92.5], // an object will help me track their progress in each topic
+        chm: [10.00,23,32.5,67.5,50,25,32.5,17.5,57.5,47.5,92.5], // an object will help me track their progress in each topic
         bio: [0],
-        eng: [-2.50,100,30,95,72.5,-12.5,20], 
+        eng: [2.50,-2.50,100,30,95,72.5,-12.5,20], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
@@ -13,9 +13,9 @@ const leaderBoard= [
     {
         posn: 3,
         name: "Ayodeji",
-        chm: [31,42.5,62,5,27.5,22.5,70,22.5,72.5,90,62.5],
+        chm: [-5.0,31,42.5,62,5,27.5,22.5,70,22.5,72.5,90,62.5],
         bio: [0],
-        eng: [10,40,30,0,52.5,5,10], 
+        eng: [15,10,40,30,0,52.5,5,10], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
@@ -33,9 +33,9 @@ const leaderBoard= [
     {
         posn: 1,
         name: "Tosin",
-        chm: [23,65,90,55,45,50,17.5,90,40,67.5],
+        chm: [50,23,65,90,55,45,50,17.5,90,40,67.5],
         bio: [0],
-        eng: [65,100,95,62.5,72.5,55,32.5], 
+        eng: [35,65,100,95,62.5,72.5,55,32.5], 
         calcScore() {
             return this.chm.reduce((a, e ) => a + e) + this.bio.reduce((a,e) => a + e) + this.eng.reduce((a,c) => a + c); // makes sense...
         }
