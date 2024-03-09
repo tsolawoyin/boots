8b3652cd90e8829d12bb97213630a5dac3f3a75d
tsolawoyin <t.olawoyin@outlook.com>
2024-03-09 20:43:44 +0100

    Removed link to contact

--- exam/new/Configure/configureHelper.js
+++ exam/new/Configure/configureHelper.js
@@ -34,7 +34,7 @@ function changeSubjects(subject, topic) {
 
 function addTime(el, time) {
   for (let i = 5; i <= time; i += 5) {
-    if (i == 45) {
+    if (i == 30) {
       el.innerHTML += `<option value="${i}" selected>${i} min</option>`  
     } else {
       el.innerHTML += `<option value="${i}">${i} min</option>`;
