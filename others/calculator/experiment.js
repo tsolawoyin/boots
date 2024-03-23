76d9cb792e8d958d547e6828a0de78c08889df6e
tsolawoyin <t.olawoyin@outlook.com>
2024-03-23 05:26:51 +0100

    Removed others

--- others/calculator/experiment.js
+++
@@ -1,6 +0,0 @@
-
-let s = [1,2,4]
-
-let u = [...s].reverse()
-console.log(s)
-console.log(u)
--- others/choose.js
+++
@@ -1,7 +0,0 @@
-
-function choose(lst) {
-    return lst[Math.floor(Math.random() * lst.length)];
-}
-
-
-console.log(choose([7,13,15,16,17,18]))
